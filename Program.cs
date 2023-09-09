a9ca867583bc919621f7b85fd02e6aa7698a2cb4
Jules C White <62078259+juleswhi@users.noreply.github.com>
2023-09-09 20:56:38 +0100

    simple and complex types

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿class Program {
+    public static void Main(string[] args) {
+        
+        string VariableOne = "Hello, World!";
+
+        char VariableTwo = 'H';
+
+        int VariableThree = 50;
+
+        double VariableFour = 3.14;
+
+        bool VariableFive = true;
+
+    }
+}
